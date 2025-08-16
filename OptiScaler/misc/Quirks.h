9c36d838a4a2b0669982492bb74a0ee419ae029b
TheRazerMD <78263903+TheRazerMD@users.noreply.github.com>
2025-08-16 23:19:12 +0200

    Update Quirks.h
    * Added WH40K Darktide nodxgi spoof Quirk
    * Added Nioh 2 Force AutoExposure Quirk
    * Fixed wrong logging value

--- OptiScaler/misc/Quirks.h
+++ OptiScaler/misc/Quirks.h
@@ -180,6 +180,13 @@ static const QuirkEntry quirkTable[] = {
     QUIRK_ENTRY("tll.exe", GameQuirk::DisableDxgiSpoofing),
     QUIRK_ENTRY("tll-l.exe", GameQuirk::DisableDxgiSpoofing),
 
+    // Nioh 2 – The Complete Edition
+    QUIRK_ENTRY("nioh2.exe", GameQuirk::ForceAutoExposure),
+
+    // Warhammer 40,000: Darktide
+    // SL spoof enough to unlock everything DLSS
+    QUIRK_ENTRY("darktide.exe", GameQuirk::DisableDxgiSpoofing),
+
     // SL spoof enough to unlock everything DLSS
     QUIRK_ENTRY("cyberpunk2077.exe", GameQuirk::CyberpunkHudlessStateOverride, GameQuirk::DisableHudfix,
                 GameQuirk::DisableDxgiSpoofing),
@@ -256,7 +263,7 @@ static void printQuirks(flag_set<GameQuirk>& quirks)
         spdlog::info("Quirk: Enabling restore compute signature on AMD/Intel");
     if (quirks & GameQuirk::DisableDxgiSpoofing)
         spdlog::info("Quirk: Dxgi spoofing disabled by default");
-    if (quirks & GameQuirk::DisableDxgiSpoofing)
+    if (quirks & GameQuirk::DisableUseFsrInputValues)
         spdlog::info("Quirk: Disable Use FSR Input Values");
     if (quirks & GameQuirk::DisableOptiXessPipelineCreation)
         spdlog::info("Quirk: Disable custom pipeline creation for XeSS");
